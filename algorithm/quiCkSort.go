e4b470249d2b8d0a01353b386b92cfa7d4d4d6b2
linzhiwei <1085101956@qq.com>
2023-03-22 17:32:47 +0800

    数组实现环形队列

---
+++ algorithm/quiCkSort.go
@@ -0,0 +1,38 @@
+package algorithm
+
+func QuickSorts(src []int, first, last int) {
+	flag := first
+	left := first
+	right := last
+	if first >= last {
+		return
+	}
+	for first < last {
+		for first < last {
+			if src[last] >= src[flag] {
+				last -= 1
+				continue
+			} else {
+				tmp := src[last]
+				src[last] = src[flag]
+				src[flag] = tmp
+				flag = last
+				break
+			}
+		}
+		for first < last {
+			if src[first] <= src[flag] {
+				first += 1
+				continue
+			} else {
+				tmp := src[first]
+				src[first] = src[flag]
+				src[flag] = tmp
+				flag = first
+				break
+			}
+		}
+	}
+	QuickSorts(src, left, flag-1)
+	QuickSorts(src, flag+1, right)
+}
